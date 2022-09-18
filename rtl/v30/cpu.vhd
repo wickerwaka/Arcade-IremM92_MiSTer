862e25b1925cf39e5c88f704d5ff15c1465e8269
Martin Donlon <github-martin@donlons.com>
2022-09-18 11:03:56 -0700

    Retore correct overflow behavior when sp is at the extremes

--- rtl/v30/cpu.vhd
+++ rtl/v30/cpu.vhd
@@ -550,6 +550,7 @@ begin
       variable bcdResultLow      : unsigned(4 downto 0);
       variable bcdResultHigh     : unsigned(4 downto 0);
       variable wordAligned       : std_logic;
+      variable varspAdjust       : unsigned(15 downto 0);
    begin
       if rising_edge(clk) then
          
@@ -1768,7 +1769,7 @@ begin
                               bus_read         <= '0';
                               bus_write        <= '1';
                               bus_be           <= "11";
-                              bus_addr         <= resize(regs.reg_ss * 16 + regs.reg_sp - 2, 20);
+                              bus_addr         <= resize(regs.reg_ss * 16 + resize(regs.reg_sp - 2,16), 20);
                               bus_datawrite    <= pushValue;
                               prefetchAllow    <= '0';
                               cpustage         <= CPUSTAGE_CHECKDATAREADY;
@@ -1777,7 +1778,7 @@ begin
                               bus_read         <= '0';
                               bus_write        <= '1';
                               bus_be           <= "01";
-                              bus_addr         <= resize(regs.reg_ss * 16 + regs.reg_sp - 2, 20);
+                              bus_addr         <= resize(regs.reg_ss * 16 + resize(regs.reg_sp - 2,16), 20);
                               bus_datawrite    <= pushValue;
                               prefetchAllow    <= '0';
                               pushFirst        <= '0';
@@ -1787,7 +1788,7 @@ begin
                            bus_read         <= '0';
                            bus_write        <= '1';
                            bus_be           <= "01";
-                           bus_addr         <= resize(regs.reg_ss * 16 + regs.reg_sp - 1, 20);
+                           bus_addr         <= resize(regs.reg_ss * 16 + resize(regs.reg_sp - 1,16), 20);
                            bus_datawrite    <= x"00" & pushValue(15 downto 8);
                            prefetchAllow    <= '0';
                            cpustage         <= CPUSTAGE_CHECKDATAREADY;
@@ -1818,7 +1819,7 @@ begin
                         if (popFirst = '1') then
                            bus_addr         <= resize(regs.reg_ss * 16 + regs.reg_sp, 20);
                         else
-                           bus_addr         <= resize(regs.reg_ss * 16 + regs.reg_sp + 1, 20);
+                           bus_addr         <= resize(regs.reg_ss * 16 + resize(regs.reg_sp + 1, 16), 20);
                         end if;
                         prefetchAllow    <= '0';
                         
