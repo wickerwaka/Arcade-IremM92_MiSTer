7fe19514873a73ff519f42409a055ad0ebafc130
Martin Donlon <github-martin@donlons.com>
2023-04-04 09:31:26 -0700

    Basic support for superior soldiers Lacking kick harness support mra needs work

--- rtl/m92_pkg.sv
+++ rtl/m92_pkg.sv
@@ -33,14 +33,16 @@ package m92_pkg;
     parameter region_t REGION_SPRITE =  '{ base_addr:'h040_0000, reorder_64:1, bram_cs:5'b00000 };
     parameter region_t REGION_TILE =    '{ base_addr:'h0c0_0000, reorder_64:0, bram_cs:5'b00000 };
     parameter region_t REGION_CRYPT =   '{ base_addr:'h000_0000, reorder_64:0, bram_cs:5'b00001 };
+    parameter region_t REGION_WIDE_SPRITE =  '{ base_addr:'h040_0000, reorder_64:0, bram_cs:5'b00000 };
 
-    parameter region_t LOAD_REGIONS[6] = '{
+    parameter region_t LOAD_REGIONS[7] = '{
         REGION_CPU_ROM,
         REGION_TILE,
         REGION_SPRITE,
         REGION_SOUND,
         REGION_CRYPT,
-        REGION_GA20
+        REGION_GA20,
+        REGION_WIDE_SPRITE
     };
 
     
